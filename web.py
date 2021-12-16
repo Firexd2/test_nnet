638528c7af5129ebbbdb8a897173ad1c9a404b75
Белоглазов Денис <denis_b@najet.ru>
2021-12-16 09:45:25 +0300

    training model

--- web.py
+++ web.py
@@ -20,14 +20,22 @@ def index():
         file = request.files['file']
         im = Image.open(file)
         im = im.resize((28, 28))
-        convert_tensor = transforms.ToTensor()
-        data = Variable(convert_tensor(im), volatile=True)
-        data = data.view(-1, 28 * 28)
-        net_out = model(data)
-        res = int(net_out.data.max(1).indices[0])
+        convert_tensor = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+        with torch.no_grad():
+            data = Variable(convert_tensor(im))
+            data = data.view(-1, 28 * 28)
+            net_out = model(data)
+            res = int(net_out.data.max(1).indices[0])
 
-        return render_template('index.html', the_title='NN', res=res)
+            print(net_out.data)
+            print(net_out.data.max(1))
+            print(res)
+
+            return str(res), 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=7010)
